62f2a86e49e494cf8da5c279a495c62676818862
Paul Bentley <paul.bentley@iesve.com>
2019-03-05 10:32:09 +0000

    Merge in changes for FlotDataPoint

--- src/FlotDotNet/FlotDataPoint.cs
+++ src/FlotDotNet/FlotDataPoint.cs
@@ -1,90 +1,38 @@
 ﻿namespace FlotDotNet
 {
-    using System.Diagnostics;
     using FlotDotNet.Infrastruture;
     using Newtonsoft.Json;
 
     /// <summary>
-    /// Represents an (x,y)-pair to be plotted on a chart, or a (label,value)-pair to be plotted on a pie chart.
+    /// Represents an x,y-pair to be plotted on a chart.
     /// </summary>
     [JsonConverter(typeof(FlotConverter))]
-    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + "()}")]
     public sealed class FlotDataPoint
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FlotDataPoint"/> class with specific values.
-        /// Use for bar, line, and scatter plots.
+        /// Initializes a new instance of the <see cref="FlotDataPoint"/> class with specific x-y values.
         /// </summary>
         /// <param name="x">The value for the x-axis</param>
         /// <param name="y">The value for the y-axis</param>
-        public FlotDataPoint(double? x, double? y)
+        public FlotDataPoint(double x, double y)
         {
             X = x;
             Y = y;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FlotDataPoint"/> class with a value but with a label.
-        /// Use this is for Pie charts.
-        /// </summary>
-        /// <param name="label">The label.</param>
-        /// <param name="value">The value.</param>
-        public FlotDataPoint(string label, double value)
-        {
-            Label = label;
-            Y = value;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FlotDataPoint"/> class with specific values.
-        /// Use for bar, line, and scatter plots.
-        /// </summary>
-        /// <param name="x">The value for the x-axis</param>
-        /// <param name="y">The value for the y-axis</param>
-        /// <param name="label">The label.</param>
-        public FlotDataPoint(double x, double y, string label)
-        {
-            // TODO: ** ADDED **
-            X = x;
-            Y = y;
-            Label = label;
         }
 
         /// <summary>
         /// Gets the value for the x-axis.
         /// </summary>
-        public double? X { get; }
+        public double X { get; }
 
         /// <summary>
         /// Gets the value for the y-axis.
         /// </summary>
-        public double? Y { get; }
+        public double Y { get; }
 
-        /// <summary>
-        /// Gets the label (e.g., for Pie charts).
-        /// </summary>
-        public string Label { get; }
-
-        /// <summary>
-        /// Serialized the <see cref="FlotDataPoint"/> to an object for JSON output.
-        /// </summary>
-        /// <returns>An object for JSON output.</returns>
-        internal object Serialize()
+        private object Serialize()
         {
-            // TODO ** ADDED **
-            if (!X.HasValue && Y.HasValue && Label != null)
-            {
-                // Pie.
-                // Braces are escaped by doubling them (http://msdn.microsoft.com/en-us/library/txafckwd.aspx).
-                // writer.WriteRaw(string.Format("{{label:'{0}', data: {1}}}", value.Label.Replace("'", "\\'"), value.Y.Value));
-                return null;
-            }
-            else
-            {
-                return new[] { X, Y };
-            }
+            return new[] { X, Y };
         }
-
-        private object DebuggerDisplay() => Serialize();
     }
 }
--- src/FlotDotNet/FlotDataPointCollectionExtensions.cs
+++ src/FlotDotNet/FlotDataPointCollectionExtensions.cs
@@ -14,22 +14,10 @@
         /// <param name="collection">The collection.</param>
         /// <param name="x">The value of the x-axis.</param>
         /// <param name="y">The value of the y-axis.</param>
-        public static void Add(this ICollection<FlotDataPoint> collection, double? x, double? y)
+        public static void Add(this ICollection<FlotDataPoint> collection, double x, double y)
         {
             GuardNotNull(collection);
             collection.Add(new FlotDataPoint(x, y));
-        }
-
-        /// <summary>
-        /// Adds a data point to the collection.
-        /// </summary>
-        /// <param name="collection">The collection.</param>
-        /// <param name="label">Label for the data point.</param>
-        /// <param name="value">Value of the data point.</param>
-        public static void Add(this ICollection<FlotDataPoint> collection, string label, double value)
-        {
-            GuardNotNull(collection);
-            collection.Add(new FlotDataPoint(label, value));
         }
 
         private static void GuardNotNull(object value)
