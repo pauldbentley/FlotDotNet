2e0a72e7158fc4d8eb6e01c22405fb2961180a35
Paul Bentley <paul.bentley@iesve.com>
2019-03-27 14:26:04 +0000

    Added FlotChartTests

---
+++ src/FlotDotNet.Tests/FlotChartTests.cs
@@ -0,0 +1,22 @@
+namespace FlotDotNet.Tests
+{
+    using Shouldly;
+    using Xunit;
+
+    public class FlotChartTests : TestClass
+    {
+        [Fact]
+        public void Empty()
+        {
+            var input = new FlotChart();
+
+            string actual = SerializeObject(input);
+            actual.ShouldContain("\"options\":{}");
+            actual.ShouldContain("\"plot\":null");
+            actual.ShouldContain("\"placeholder\":\"#chart_placeholder\"");
+            actual.ShouldContain("\"plotChart\":function");
+            actual.ShouldContain("\"plotData\":function");
+            actual.ShouldContain("\"data\":{}");
+        }
+    }
+}
